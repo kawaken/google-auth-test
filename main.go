c5dacd4708eb35277fb335d7fec864901c61d526
Kentaro Kawano <kentaro.kawano@gmail.com>
2015-06-29 12:46:20 +0900

    一通り認証できるようになった

--- main.go
+++ main.go
@@ -25,6 +25,7 @@ type Config struct {
 	CLIENT_SECRET string
 	AccessToken   string
 	RefreshToken  string
+	ExpiredAt     time.Time
 }
 
 type DeviceToken struct {
@@ -50,25 +51,19 @@ func loadConf() (conf *Config, err error) {
 	return
 }
 
-func writeConf(conf *Config) error {
+func writeConf(conf *Config) (err error) {
 
 	var buffer bytes.Buffer
 	encoder := toml.NewEncoder(&buffer)
-	err := encoder.Encode(conf)
+	err = encoder.Encode(conf)
 	if err != nil {
-		return err
-	}
-	err = ioutil.WriteFile("conf.toml", buffer.Bytes(), os.ModePerm)
-	return err
-}
-
-func main() {
-
-	conf, err := loadConf()
-	if err != nil {
-		fmt.Println(err)
 		return
 	}
+	err = ioutil.WriteFile("conf.toml", buffer.Bytes(), os.ModePerm)
+	return
+}
+
+func initAccessToken(conf *Config) {
 
 	values := url.Values{}
 	values.Add("client_id", conf.CLIENT_ID)
@@ -132,6 +127,7 @@ func main() {
 			fmt.Println("\nVerified")
 			conf.AccessToken = at.AccessToken
 			conf.RefreshToken = at.RefreshToken
+			conf.ExpiredAt = time.Now().Add(time.Duration(at.ExpiresIn) * time.Second)
 
 			err := writeConf(conf)
 			if err != nil {
@@ -143,5 +139,73 @@ func main() {
 	}
 
 	fmt.Println("Unauthorized. Retry")
+
+}
+
+func refreshAccessToken(conf *Config) {
+	fmt.Println("refresh!!!!")
+
+	values := url.Values{}
+	values.Add("client_id", conf.CLIENT_ID)
+	values.Add("client_secret", conf.CLIENT_SECRET)
+	values.Add("refresh_token", conf.RefreshToken)
+	values.Add("grant_type", "refresh_token")
+
+	resp, err := http.PostForm(POLLING_URL, values)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var at AuthToken
+	err = json.Unmarshal(b, &at)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if at.Error == "" && at.AccessToken != "" {
+		fmt.Println("\nVerified")
+		conf.AccessToken = at.AccessToken
+		if at.RefreshToken != "" {
+			conf.RefreshToken = at.RefreshToken
+		}
+		conf.ExpiredAt = time.Now().Add(time.Duration(at.ExpiresIn) * time.Second)
+
+		err := writeConf(conf)
+		if err != nil {
+			fmt.Println(err)
+		}
+		return
+	}
+}
+
+func main() {
+
+	conf, err := loadConf()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if conf.AccessToken == "" {
+		initAccessToken(conf)
+		return
+	}
+
+	if conf.ExpiredAt.Before(time.Now()) {
+		refreshAccessToken(conf)
+		return
+	}
+
+	fmt.Println("Nothing to be done")
 
 }
